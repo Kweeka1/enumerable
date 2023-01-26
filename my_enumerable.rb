3d066be6cb8101d270e3911b402191b3eb5f0c32
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2023-01-26 18:35:13 +0200

    implement any and all to filter array

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -9,4 +9,22 @@ module MyEnumerable
     end
     true
   end
+  def any?
+    raise Exception, ERROR_MESSAGE unless block_given?
+  
+    each_num do |num|
+      return true if yield num
+    end
+    false
+  end
+  
+  def filter
+    raise Exception, ERROR_MESSAGE unless block_given?
+  
+    filtered = []
+    each_num do |num|
+      filtered.push num if yield num
+    end
+    filtered
+  end
 end
