6916f7cc72ff3062293b5ab49d118ab4b86adcaa
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2023-01-26 18:41:38 +0200

    uncomment any and filter method

--- mylist.rb
+++ mylist.rb
@@ -15,5 +15,5 @@ end
 c = MyList.new([1, 2, 4, 3, 5, 7])
 c.each_num { |val| puts val }
 puts c.all? { |val| val < 5 }
-#puts c.any? { |val| val > 1 }
-#puts c.filter { |val| val.even? }
+puts c.any? { |val| val > 1 }
+puts c.filter { |val| val.even? }
